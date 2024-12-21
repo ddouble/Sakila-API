9e0bfda093c27501c445dd62aa6fb9e03ad6f195
Alan <ddouble.cn@gmail.com>
2024-12-21 08:21:34 +0100

    Add InlineMap.of to implement an fixed order version of Map.of

---
+++ src/main/java/org/example/demo/utils/InlineMap.java
@@ -0,0 +1,28 @@
+package org.example.demo.utils;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class InlineMap {
+
+    @SafeVarargs
+    public static <K, V> Map<K, V> of(K key1, V value1, Object... otherPairs) {
+        if (otherPairs.length % 2 != 0) {
+            throw new IllegalArgumentException("Other pairs must be in key-value format");
+        }
+
+        Map<K, V> map = new LinkedHashMap<>();
+        map.put(key1, value1);
+
+        for (int i = 0; i < otherPairs.length; i += 2) {
+            @SuppressWarnings("unchecked")
+            K key = (K) otherPairs[i];
+            @SuppressWarnings("unchecked")
+            V value = (V) otherPairs[i + 1];
+            map.put(key, value);
+        }
+
+        return map;
+    }
+}
+
